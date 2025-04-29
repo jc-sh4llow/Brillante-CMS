d287926a01148d55b14f3b552141d2d86ba39b67
jc-sh4llow <nanaymo0914@gmail.com>
2025-04-29 21:22:11 +0800

    added auto increment to PRIMARY KEY appointment_id

--- Table_8-9-10_Task-2.sql
+++ Table_8-9-10_Task-2.sql
@@ -13,13 +13,27 @@ CREATE TABLE appointments (
     patient_id NUMBER(5),
     dentist_id NUMBER(5) NOT NULL,
     service_id NUMBER(5) NOT NULL,
-    appointment_date DATE NOT NULL CHECK (appointment_date >= SYSDATE),
+    appointment_date DATE NOT NULL,
     status VARCHAR2(9) NOT NULL CHECK (status IN ('Scheduled', 'Completed', 'Cancelled')),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL/*,
     FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
     FOREIGN KEY (dentist_id) REFERENCES dentists(dentist_id),
     FOREIGN KEY (service_id) REFERENCES services(service_id)*/
 );
+
+CREATE SEQUENCE appointment_seq
+    START WITH 1
+    INCREMENT BY 1
+    NOCACHE;
+
+CREATE OR REPLACE TRIGGER trg_appointments_ai
+    BEFORE INSERT ON appointments
+    FOR EACH ROW
+    BEGIN
+        IF :NEW.appointment_id IS NULL THEN
+            SELECT appointment_seq.NEXTVAL INTO :NEW.appointment_id FROM dual;
+        END IF;
+    END;
 
 CREATE TABLE payments (
     payment_id NUMBER(5) PRIMARY KEY,
@@ -28,8 +42,8 @@ CREATE TABLE payments (
     payment_method VARCHAR2(13) NOT NULL CHECK (payment_method IN ('Cash', 'GCash', 'Bank Transfer')),
     status VARCHAR2(8) NOT NULL CHECK (status IN('Pending', 'Paid', 'Refunded')),
     receipt_number NUMBER NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL/*,
-    FOREIGN KEY (patient_id) REFERENCES patients(patient_id)*/
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id)
 );
 
 CREATE TABLE visit_history(
@@ -37,9 +51,9 @@ CREATE TABLE visit_history(
     patient_id NUMBER(5) NOT NULL,
     dentist_id NUMBER(5) NOT NULL,
     appointment_id NUMBER(5),
-    FOREIGN KEY (patient_id) REFERENCES patients(patient_id)/*,
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
     FOREIGN KEY (dentist_id) REFERENCES dentists(dentist_id),
-    FOREIGN KEY (appointment_id) REFERENCES appointments(appointment_id)*/
+    FOREIGN KEY (appointment_id) REFERENCES appointments(appointment_id)
 );
 
 CREATE TABLE patients(
@@ -71,3 +85,16 @@ CREATE TABLE patients(
     FOREIGN KEY (dental_history_id) REFERENCES dental_history(dental_history_id),
     FOREIGN KEY (medical_history_id) REFERENCES medical_history(medical_history_id)
 );
+
+CREATE TABLE dental_history(
+    dental_history_id NUMBER(5) PRIMARY KEY,
+    previous_dentist VARCHAR2(250)
+    previous_dental_clinic VARCHAR2(100),
+    reason_of_last_dental_visit VARCHAR2(250),
+    last_dental_visit DATE,
+    clicking_lock_jaw CHAR(1) CHECK (clicking_lock_jaw IN('Y', 'N')),
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+
+INSERT INTO appointments ()
